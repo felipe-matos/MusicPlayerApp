c7cede9fa518aabd3559f95db8747ff8919f66ca
felipe <felipematosapps@gmail.com>
2024-10-06 08:11:22 -0300

    Music player app

---
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.musicplayerapp
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mp : MediaPlayer
+    private lateinit var btnPlay : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnPlay = findViewById(R.id.btnPlay)
+
+        mp = MediaPlayer.create(this,R.raw.happyrock)
+        mp.start()
+        btnPlay.setImageResource(R.drawable.baseline_pause_chart_24)
+
+        btnPlay.setOnClickListener{
+
+            if(mp.isPlaying){
+
+                mp.pause()
+                btnPlay.setImageResource(R.drawable.baseline_arrow_right_24)
+            }else {
+                mp.start()
+                btnPlay.setImageResource(R.drawable.baseline_pause_chart_24)
+
+            }
+
+
+        }
+
+    }
+}
